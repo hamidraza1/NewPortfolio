8781abc6602a154d247884be4ac6fce3203c5f0d
hamidraza1 <hrazaa306@gmail.com>
2020-08-30 23:26:17 +0200

    issues fixed

--- public/component---src-pages-page-2-js-b7991b5628f13208b9eb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),c=t("Wbzz");n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-b7991b5628f13208b9eb.js.map
